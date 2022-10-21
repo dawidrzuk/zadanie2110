ec9bfce3602542d3a01df97052885672af74861c
Ok-tvn <oktawiansegieda@gmail.com>
2022-10-21 14:56:34 +0200

    przyciski layout zrobiony

--- app/src/main/java/com/example/dawidrzuk2110/MainActivity.kt
+++ app/src/main/java/com/example/dawidrzuk2110/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.dawidrzuk2110
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val etykieta = findViewById<TextView>(R.id.textView)
+        val napis = findViewById<EditText>(R.id.editTextTextPersonName)
+
     }
 }
